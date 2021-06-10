aebe65b9078ac8c6547914ad097436a7b9dc41aa
Matheus Penchel <matheus.penchel@gmail.com>
2021-06-10 09:36:19 -0300

    Solved the N+1 queries and added examples on more complex associated records, and also how to do 1 query instead of 1 / association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,30 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    # @books = Book.all
+    @books = Book.includes(:author).all
   end
 end
+
+# Book -> belongs_to -> Author -> has_many Address -> belongs_to Country
+# @books = Book.includes(author: {addresses: :country})
+
+# Book -> belongs_to -> Author -> has_many Address
+# @books = Book.includes(author: :addresses)
+# books.each do |book|
+# book.author.addresses.each do |address|
+# address.street
+# address.number
+
+# @books = Book.joins(:author).select("books.title, authors.name") # this is faster ):
+# SELECT books.title, authors.name
+# FROM books
+# JOIN authors ON authors.id = books.author_id
+
+# Wizard (has_many :enemies, has_many :spells, belongs_to :adventuring_group)
+# @wizards = Wizard.includes(:enemies, :spells, :adventuring_group)
+# @wizards.each do |wizard|
+# <ul> wizard.enemies.each do |enemy| <li> enemy.name </li> </ul>
+# <ul> wizard.spells.each do |spell| <li> spell.name </li> </ul>
+# wizard.adventuring_group.each do |adventurers|
+# <ul> adventurers.each do |adventurer| <li> adventurer.name </li> </ul>
+
